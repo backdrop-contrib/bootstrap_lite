eddc1211ea0f1bdeddbd05f80e2080c3dcad1b31
Gor Martsen <Gor.martsen@gmail.com>
2015-10-16 15:48:53 -0400

    First release

---
+++ README.md
@@ -0,0 +1,24 @@
+#Bootstrap - Backdrop theme 
+*ported from [Drupal Bootstrap theme 7.x-3.0](https://www.drupal.org/project/bootstrap)*
+
+1. [BootstrapCDN](http://bootstrapcdn.com/) based.
+2. [Bootswatch](http://bootswatch.com) support (via BootstrapCDN) included. Easy to pick a Bootswatch free theme.
+3. Font awesome support included.
+4. Other tweaks:
+  - Navbar settings (fixed, static, top, bottom)
+  - Navbar user menu with cog icon
+  - Breadcrumbs tweaks
+  - Content fluid or fixed width
+  
+## Excluded from code
+  - Starter kit. But you still can create sub theme. See [Developing themes](https://api.backdropcms.org/developing-themes)
+  - Tooltip. Feature is here, but you need follow [documentation](http://getbootstrap.com/javascript/#tooltips) to make it work.
+  - Popovers.  Feature is here, but you need follow [documentation](http://getbootstrap.com/javascript/#popovers) to make it work.
+  - Anchors settings. I believe this one need to be done via module.
+
+## Roadmap
+  1.x-3.3.5.2
+   - Tooltops, Popovers implementation in a best way to integrate with backdrop.
+
+##Thanks to drupal module Authors
+http://drupal.org/node/259843/committers
---
+++ theme-settings.php
@@ -0,0 +1,216 @@
+<?php
+/**
+ * @file
+ * theme-settings.php
+ *
+ * Theme settings file for Bootstrap.
+ */
+
+function bootstrap_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL) {
+
+  if (isset($form_id)) {
+    return;
+  }
+
+  $form['bootstrap'] = array(
+    '#type' => 'vertical_tabs',
+    '#attached' => array(
+      'js'  => array(drupal_get_path('theme', 'bootstrap') . '/js/bootstrap.admin.js'),
+    ),
+    '#prefix' => '<h2><small>' . t('Bootstrap Settings') . '</small></h2>',
+    '#weight' => -10,
+  );
+  // Components.
+  $form['tweaks'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Tweaks'),
+    '#group' => 'bootstrap',
+  );
+
+  $form['tweaks']['bootstrap_container'] = array(
+    '#type' => 'select',
+    '#title' => t('Container type'),
+    '#default_value' => theme_get_setting('bootstrap_container', 'bootstrap'),
+    '#description' => t('Switch between full width (fluid) or fixed (max 1170px) width.'),
+    '#options' => array(
+      'container' => t('Fixed'),
+      'container-fluid' => t('Fluid'),
+    )
+  );
+
+  $form['tweaks']['bootstrap_datetime'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Show "XX time ago".'),
+    '#default_value' => theme_get_setting('bootstrap_datetime', 'bootstrap'),
+    '#description' => t('If enabled, replace date output for nodes and comments by "XX time ago".'),
+  );
+
+  $form['breadcrumbs'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Breadcrumbs'),
+    '#collapsible' => TRUE,
+    '#collapsed' => TRUE,
+    '#group' => 'bootstrap',
+  );
+  $form['breadcrumbs']['bootstrap_breadcrumb_home'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Show "Home" breadcrumb link'),
+    '#default_value' => theme_get_setting('bootstrap_breadcrumb_home', 'bootstrap'),
+    '#description' => t('If your site has a module dedicated to handling breadcrumbs already, ensure this setting is enabled.'),
+  );
+  $form['breadcrumbs']['bootstrap_breadcrumb_title'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Show current page title at end'),
+    '#default_value' => theme_get_setting('bootstrap_breadcrumb_title', 'bootstrap'),
+    '#description' => t('If your site has a module dedicated to handling breadcrumbs already, ensure this setting is disabled.'),
+  );
+
+  $form['navbar'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Navbar'),
+    '#description' => t('Navigation bar settings.'),
+    '#collapsible' => TRUE,
+    '#collapsed' => TRUE,
+    '#group' => 'bootstrap',
+  );
+  $form['navbar']['bootstrap_navbar_position'] = array(
+    '#type' => 'select',
+    '#title' => t('Navbar Position'),
+    '#description' => t('Select your Navbar position.'),
+    '#default_value' => theme_get_setting('bootstrap_navbar_position', 'bootstrap'),
+    '#options' => array(
+      'static-top' => t('Static Top'),
+      'fixed-top' => t('Fixed Top'),
+      'fixed-bottom' => t('Fixed Bottom'),
+    ),
+    '#empty_option' => t('Normal'),
+  );
+  
+  $form['navbar']['bootstrap_navbar_menu_position'] = array(
+    '#type' => 'select',
+    '#title' => t('Navbar Menu Position'),
+    '#description' => t('Select your Navbar Menu position.'),
+    '#default_value' => theme_get_setting('bootstrap_navbar_menu_position', 'bootstrap'),
+    '#options' => array(
+      'navbar-left' => t('Left'),
+      'navbar-right' => t('Right'),
+    ),
+    '#empty_option' => t('Normal'),
+  );
+  
+  $form['navbar']['bootstrap_navbar_inverse'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Inverse navbar style'),
+    '#description' => t('Select if you want the inverse navbar style.'),
+    '#default_value' => theme_get_setting('bootstrap_navbar_inverse', 'bootstrap'),
+  );
+
+  $form['navbar']['bootstrap_navbar_user_menu'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Add cog with user-menu'),
+    '#description' => t('Select if you want cog style right pulled popup menu.'),
+    '#default_value' => theme_get_setting('bootstrap_navbar_user_menu', 'bootstrap'),
+  );
+
+  backdrop_add_css(backdrop_get_path('theme', 'bootstrap') . '/css/settings.css');
+  $form['bootstrap_cdn'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('BootstrapCDN settings'),
+    '#description' => t('Use !bootstrapcdn to serve the Bootstrap framework files. Enabling this setting will prevent this theme from attempting to load any Bootstrap framework files locally. !warning', array(
+      '!bootstrapcdn' => l(t('BootstrapCDN'), 'http://bootstrapcdn.com', array(
+        'external' => TRUE,
+      )),
+    '!warning' => '<div class="alert alert-info messages info"><strong>' . t('NOTE') . ':</strong> ' . t('While BootstrapCDN (content distribution network) is the preferred method for providing huge performance gains in load time, this method does depend on using this third party service. BootstrapCDN is under no obligation or commitment to provide guaranteed up-time or service quality for this theme. If you choose to disable this setting, you must provide your own Bootstrap source and/or optional CDN delivery implementation.') . '</div>',
+    )),
+    '#group' => 'bootstrap',
+    '#collapsible' => TRUE,
+    '#collapsed' => TRUE,    
+  );
+  // BootstrapCDN.
+
+  $form['bootstrap_cdn']['bootstrap_cdn'] = array(
+    '#type' => 'select',
+    '#title' => t('BootstrapCDN version'),
+    '#options' => drupal_map_assoc(array(
+      '3.3.5',
+    )),
+    '#default_value' => theme_get_setting('bootstrap_cdn', 'bootstrap'),
+    '#empty_option' => t('Disabled'),
+    '#empty_value' => NULL,
+  );
+  
+  $form['bootstrap_cdn']['bootstrap_font_awesome'] = array(
+    '#type' => 'select',
+    '#title' => t('Font Awesome version'),
+    '#options' => drupal_map_assoc(array(
+      '4.4.0',
+    )),
+    '#default_value' => theme_get_setting('bootstrap_font_awesome', 'bootstrap'),
+    '#empty_option' => t('Disabled'),
+    '#empty_value' => NULL,
+  );
+
+  $bootswatch_themes = array();
+  $default_theme_details = array(
+    'name' => t('Default'),
+    'description' => t('Pure Bootstrap CSS'),
+    'thumbnail' => base_path() . backdrop_get_path('theme', 'bootstrap') . '/preview.jpg',
+  );
+  
+  $bootswatch_themes[''] = bootstrap_bootswatch_template($default_theme_details);
+  $request = drupal_http_request('http://api.bootswatch.com/3/');
+  if ($request && $request->code === '200' && !empty($request->data)) {
+    if (($api = backdrop_json_decode($request->data)) && is_array($api) && !empty($api['themes'])) {
+      foreach ($api['themes'] as $bootswatch_theme) {
+        $bootswatch_themes[strtolower($bootswatch_theme['name'])] = bootstrap_bootswatch_template($bootswatch_theme);
+      }
+    }
+  }  
+  $form['bootswatch'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Bootswatch theme'),
+    '#collapsible' => TRUE,
+    '#collapsed' => TRUE,
+    '#group' => 'bootstrap',
+    '#description' => t('Use !bootstrapcdn to serve a Bootswatch Theme. Choose Bootswatch theme here.', array(
+      '!bootstrapcdn' => l(t('BootstrapCDN'), 'http://bootstrapcdn.com', array(
+        'external' => TRUE,
+      )),
+    )),
+  );
+
+    
+  $form['bootswatch']['bootstrap_bootswatch'] = array(
+    '#type' => 'radios',
+    '#default_value' => theme_get_setting('bootstrap_bootswatch', 'bootstrap'),
+    '#options' => $bootswatch_themes,
+    '#empty_option' => t('Disabled'),
+    '#empty_value' => NULL,
+    '#prefix' => '<div class="section-preview">',
+    '#suffix' => '</div>',
+  );
+  if (empty($bootswatch_themes)) {
+    $form['bootswatch']['bootstrap_bootswatch']['#prefix'] = '<div class="alert alert-danger messages error"><strong>' . t('ERROR') . ':</strong> ' . t('Unable to reach Bootswatch API. Please ensure the server your website is hosted on is able to initiate HTTP requests.') . '</div>';
+  }
+}
+
+function bootstrap_bootswatch_template($bootswatch_theme){
+  $output = '<div class="preview">';
+  
+  if(isset($bootswatch_theme['thumbnail'])){
+    $output .= '<div class="image">
+      <img src="' . $bootswatch_theme['thumbnail']. '" class="img-responsive" alt="' . $bootswatch_theme['name'] . '">
+    </div>';
+  }
+  $output .= '<div class="options">
+      <h3>' . $bootswatch_theme['name'] . '</h3>
+      <p>' . $bootswatch_theme['description'] . '</p>';
+  if(isset($bootswatch_theme['preview'])){
+    $output .= '<div class="btn-group"><a class="btn btn-info" href="' . $bootswatch_theme['preview'] . '" target="_blank">' . t('Preview') . '</a></div>';
+  }else{
+    $output .= '<div class="btn-group"><a class="btn btn-default disabled" href="#" target="_blank">' . t('No preview') . '</a></div>';
+  }
+  $output .= '</div>
+  </div>';
+  return $output;
+}
